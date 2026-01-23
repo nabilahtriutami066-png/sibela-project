0a3b74ca7f25068f3498be62fa67a3e632757cb9
nabilahtriutami066-png <nabilahtriutami066@gmail.com>
2026-01-23 20:24:03 +0700

    Ngubah warna

--- constants/color.h
+++ constants/color.h
@@ -3,10 +3,11 @@
 
 #include "../libs/headers/raylib.h"
 
-#define PRIMARY (Color){0, 128, 255, 255}
-#define SECONDARY (Color){58, 71, 80, 255}
-#define TERTIARY (Color){48, 56, 65, 255}
-#define SIBELAWHITE (Color){243, 243, 243, 255}
-#define DANGER (Color){252, 61, 50, 255}
+#define PRIMARY      (Color){18, 64, 140, 255}    // Bold deep blue (power)
+#define SECONDARY    (Color){36, 98, 180, 255}    // Strong royal blue
+#define TERTIARY     (Color){12, 22, 40, 255}     // Very dark navy (background)
+#define SIBELAWHITE  (Color){225, 235, 245, 255}  // Cool soft off-white
+#define DANGER       (Color){190, 55, 65, 255}    // Muted strong red
+
 
 #endif
